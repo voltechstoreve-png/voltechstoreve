f33768cd453e0d781ed9bfd1060e535f37159de5
Tu Nombre <tu-email@ejemplo.com>
2026-09-13 20:13:07 -0400

    agrega redirects de Vercel a dominio

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'voltechstoreve-6nuj-git-main-voltechstoreve-pngs-projects.vercel.app',
+          },
+        ],
+        destination: 'https://voltechstoreve.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'voltechstoreve-6nuj.vercel.app',
+          },
+        ],
+        destination: 'https://voltechstoreve.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
