39a0d267ed11b0eef47ac94a76905dda48e673d6
voltechstoreve-png <voltechstore.ve@gmail.com>
2026-09-16 11:32:45 -0400

    Fix JSX syntax for rendering product characteristics

--- app/catalogo/page.jsx
+++ app/catalogo/page.jsx
@@ -2527,6 +2527,7 @@
                             )}
                           </div>
                         </div>
+                      </div>
                       )}
 
                       {/* Características */}
